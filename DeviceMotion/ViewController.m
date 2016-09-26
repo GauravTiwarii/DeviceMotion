b675241abbc7aab9aa358c49062c8505909155fe
Love <Love@love.local>
2016-09-27 03:25:10 +0530

    2.0

--- DeviceMotion/ViewController.m
+++ DeviceMotion/ViewController.m
@@ -15,6 +15,8 @@
 
 @property (strong,nonatomic)CMMotionManager *manager;
 
+@property(strong,nonatomic)NSArray *images;
+
 @end
 
 @implementation ViewController
@@ -23,7 +25,10 @@
     [super viewDidLoad];
     self.manager = [[CMMotionManager alloc]init];
     
-    self.imageView.image =[UIImage imageNamed:@"clownfish.jpg"];
+    
+    self.images = @[[UIImage imageNamed:@"abstract.jpg"],[UIImage imageNamed:@"sample.jpg"],[UIImage imageNamed:@"clownfish.jpg"],[UIImage imageNamed:@"glass.jpg"]];
+    [self chooseImage:0.0];
+    
     [self.manager startDeviceMotionUpdates];
     
     self.manager.accelerometerUpdateInterval = 0.01;
@@ -31,28 +36,65 @@
     ViewController* __weak weakSelf = self;
     
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    CMAttitudeReferenceFrame frame = CMAttitudeReferenceFrameXArbitraryCorrectedZVertical;
     
-    [self.manager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *motionData, NSError *error) {
+    [self.manager startDeviceMotionUpdatesUsingReferenceFrame:frame toQueue:queue withHandler:^(CMDeviceMotion *motionData, NSError *error) {
         
-        double x= motionData.gravity.x;
-        double y = motionData.gravity.y;
-        //double z = motionData.gravity.z;
+        double yaw = motionData.attitude.yaw;
+
         
-        double rotation = -atan2(x, -y);
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
         
-        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-            
-            weakSelf.imageView.transform = CGAffineTransformMakeRotation(rotation);
-            
-            
-        }];
-        
-    }];
+                    weakSelf.imageView.transform = CGAffineTransformMakeRotation(yaw);
+                    
+                    [self chooseImage:yaw];
+                    
+                }];
+                
+            }];
+
+    
+    
+    
+//    You do this part when reference Frame is not the part of concern
+//   // [self.manager startDeviceMotionUpdatesToQueue:queue withHandler:^(CMDeviceMotion *motionData, NSError *error) {
+//        
+//        double x= motionData.gravity.x;
+//        double y = motionData.gravity.y;
+//        //double z = motionData.gravity.z;
+//        
+//        double rotation = -atan2(x, -y);
+//        
+//        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//            
+//            weakSelf.imageView.transform = CGAffineTransformMakeRotation(rotation);
+//            
+//            
+//        }];
+//        
+//    }];
     
 }
 
 
-
+-(void)chooseImage:(double)yaw{
+    
+    if(yaw<=M_PI_4){
+        self.imageView= self.images[0];
+    }
+    else if(yaw>=-3.0*M_PI_4){
+        self.imageView.image = self.images[1];
+    }
+    else {
+        if(yaw<=3.0*M_PI_4){
+            
+            self.imageView.image = self.images[3];
+        }
+        else{
+            self.imageView.image = self.images[2];
+        }
+    }
+}
 
 
 @end
